b220710e2e3a412795341019032c42c843611aec
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-17 15:29:04 +0330

    build components

--- src/component/notes/NoteCard.jsx
+++ src/component/notes/NoteCard.jsx
@@ -1,7 +1,22 @@
-export default function NoteCard() {
+import { useNoteStore } from "../../store/notesStore"
+
+export default function NoteCard({ note }) {
+    const deleteNote=useNoteStore((state)=> state.deleteNote)
+
     return (
         <>
-            <h1>NoteCard</h1>
+            <div className="bg-white p-4 rounded-xl shadow-md border border-gray-200 flex flex-col gap-3">
+                <div className="flex justify-between items-start">
+                    <h2 className="text-lg font-semibold text-gray-800 ">{note.title}</h2>
+                    <button
+                        onClick={()=> deleteNote(note.id)}
+                        className="text-red-500 hover:text-red-700 transition"
+                    >X</button>
+                </div>
+
+                <p className="text-xs text-gray-400 ">{note.text}</p>
+                <span className="text-xs text-gray-400">{formData(note.createdAt) }</span>
+</div>
         </>
     )
 }
--- src/component/notes/NoteList.jsx
+++ src/component/notes/NoteList.jsx
@@ -1,7 +1,21 @@
+import { useNoteStore } from "../../store/notesStore"
+import NoteCard from "./NoteCard"
+
 export default function NoteList() {
+
+const notes=useNoteStore((state)=>state.notes)
+
+    if (notes.length === 0) {
+        return (
+            <p className="text-center text-gray-500 mt-6">There is no Note here!</p>
+        )
+    }
+    
     return (
         <>
-        <h1>NoteList</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+                {notes.map((note)=>(<NoteCard key={note.id} note={note} />))}
+        </div>
         </>
     )
 } 
